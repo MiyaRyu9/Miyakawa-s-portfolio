177dbeded5f530357790a1908e9736794cc46516
Ryusei Miyakawa <miyakawa928@gmail.com>
2025-04-24 22:12:33 +0900

    error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./components/About";
-import Blog from "./components/Blog";
+// import Blog from "./components/Blog";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
