889b670d398eeaa346c22299babff95074a11053
Ryusei Miyakawa <miyakawa928@gmail.com>
2025-04-24 22:14:27 +0900

    error

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="fixed top-0 w-full bg-black/70 backdrop-blur-md z-50 shadow-md">
       <div className="max-w-6xl mx auto px-4 py-3 flex justify-between items-center">
         <h1 className ="text-xl font-bold text-white">
-          Miyakawa&apos; Portfolio
+          Miyakawa Portfolio
         </h1>
         <nav className="space-x-10">
           <Link href="/about" className="text-gray-200 hover:text-blue-400 transition">About</Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home(){
   return(
     <div className="bg-black text-white">
       <Head>
-        <title>Miyakawa's portfolio</title>
+        <title>Miyakawa portfolio</title>
 
       </Head>
       <Header />
