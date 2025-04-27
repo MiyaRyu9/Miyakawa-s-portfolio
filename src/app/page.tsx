df146de85b1a2232b7429da0514346f0c953516c
Ryusei Miyakawa <miyakawa928@gmail.com>
2025-04-27 11:06:59 +0900

    twice change favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Head from "next/head";
-import { Main } from "next/document";
 
 export default function Home(){
   return(
